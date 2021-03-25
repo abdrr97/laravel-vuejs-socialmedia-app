ab7d39d10b1f9f960870f796e156745b78d363e6
Abdrr97 <abdrr97@gmail.com>
2021-03-25 15:22:32 +0100

    moving routes for api.php to web.php entil i figure this out

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostCommentController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +17,28 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::group(['prefix' => 'api'], function ()
+{
+    Route::group(['prefix' => 'post'], function ()
+    {
+        Route::get('list', [PostController::class, 'list']);
+        Route::post('create', [PostController::class, 'store']);
+        Route::post('{post}/like', [PostController::class, 'like']);
+        Route::post('{post}/delete', [PostController::class, 'delete_post']);
+        Route::post('{post}/comments', [PostCommentController::class, 'create']);
+    });
+    Route::group(['prefix' => 'user'], function ()
+    {
+        Route::get('logged_in_user', [UserController::class, 'logged_in_user']);
+        Route::post('{user}/follow', [UserController::class, 'follow']);
+        Route::post('{user}/unfollow', [UserController::class, 'unfollow']);
+    });
+
+    Route::get('feed', [PostController::class, 'feed']);
+});
+
+
 
 Route::get('/', function ()
 {
